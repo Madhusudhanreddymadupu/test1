acce6ec6feb64c0bc61cadcb63fd06bda7482ce2
Madhusudhanreddymadupu <madhusudhanreddy19.madupu@gmil.com>
2024-05-24 12:20:27 +0530

    added

---
+++ method.java
@@ -0,0 +1,20 @@
+Class Main extends Demo
+{
+	public static void main (String args [])
+	{
+		Main main=new Main();
+		main.m1();
+	}
+	public void m1()
+	{
+		System.out.println("hii");
+	}
+
+class Demo
+{
+	public void m1()
+	{
+		System.out.println("hlo");
+	}
+}
+}
